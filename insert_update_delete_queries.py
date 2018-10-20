21ca1b352497d230e7a19a84fe3cb54cea046a9d
ishwar <ishwar@screen-magic.com>
2018-10-20 18:30:28 +0530

    added the queries for delete, update and select the data from Database using SQLAlchemy

--- insert_update_delete_queries.py
+++ insert_update_delete_queries.py
@@ -25,4 +25,27 @@ insert_to_book = Book(
 )
 
 session.add(insert_to_book)
+
+
+#Delete from Student where id=1
+session.query(Student).filter(Student.id==1).delete()
+
+#Delete from Book where id=1
+session.query(Book).filter(Book.id==1).delete()
+
 session.commit()
+
+
+#Update Student
+session.query(Student).filter(Student.id==2).update({"name": "Tom Jerry"})
+session.commit()
+
+
+#select Student
+result = session.query(Student).filter_by(id=2).first()
+print result.name
+
+
+#select Book
+book_result = session.query(Book).filter_by(id=2).all()
+print book_result[0].title
